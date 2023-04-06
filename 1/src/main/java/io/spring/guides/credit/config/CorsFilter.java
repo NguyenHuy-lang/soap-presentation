8934834edf7dd02a69a05d91e5b9aac0b7ea118f
NguyenHuy-lang <huynguyend19ptit@gmail.com>
2023-04-06 18:39:41 +0700

    add class CorsFilter

---
+++ 1/src/main/java/io/spring/guides/credit/config/CorsFilter.java
@@ -0,0 +1,31 @@
+package io.spring.guides.credit.config;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        if (!"OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            chain.doFilter(req, res);
+        } else {
+            // do not continue with the filter chain for options requests
+        }
+    }
+}
